1cc5832c6b11c1037a20dd189c8839a9c40fade4
ksv0 <128997942+ksv0@users.noreply.github.com>
2023-09-08 13:50:46 +0400

    Done

--- src/main/java/fe/Main.java
+++ src/main/java/fe/Main.java
@@ -1,5 +1,8 @@
 package fe;
-
+/**
+ * Кардаполов СВ
+ * Разработчик | Программист | будни вечер | 4720 | 13.06.23
+ */
 public class Main {
     public static void main(String[] args) {
         GameProcess gameProcess = new GameProcess();
